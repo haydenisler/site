8ac571784ee0be9140d967eb709ecc03c383e83b
Hayden Isler <me@haydenisler.com>
2022-02-28 07:34:34 -0600

    style: add color theme

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,7 @@
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
-  return (
-    <div className="min-h-screen flex items-center justify-center">
-      Hello, world!
-    </div>
-  );
+  return <div className="flex flex-col min-h-screen p-4 bg-rich-black" />;
 };
 
 export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,12 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      'rich-black': '#0D1321',
+      salsa: '#E54B4B',
+      battleship: '#808782',
+      cadet: '#586F7C'
+    },
     fontFamily: { 
       sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       serif: [...defaultTheme.fontFamily.serif],
