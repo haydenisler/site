dda4946039883c107463c43741d821f46e2a10c6
Hayden Isler <me@haydenisler.com>
2022-02-27 20:55:13 -0600

    chore: add _document.tsx + Inter font

---
+++ pages/_document.tsx
@@ -0,0 +1,40 @@
+import Document, {
+  DocumentContext,
+  DocumentInitialProps,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from 'next/document';
+
+class MyDocument extends Document {
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render(): JSX.Element {
+    return (
+      <Html lang="en">
+        <Head>
+          <meta charSet="utf-8" />
+          <meta name="theme-color" content="#000000" />
+          <meta
+            name="description"
+            content="Web site created using create-next-app"
+          />
+          <link rel="icon" href="/favicon.ico" />
+          <link href="https://rsms.me/inter/inter.css" rel="stylesheet"></link>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
