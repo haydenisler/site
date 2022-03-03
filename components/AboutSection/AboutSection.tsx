8488c4e5e00a76b96207a4904bb5398caa2e9dff
Hayden Isler <me@haydenisler.com>
2022-03-03 12:50:42 -0600

    refactor: AboutSection component

---
+++ components/AboutSection/AboutSection.tsx
@@ -0,0 +1,18 @@
+import type { FC } from 'react';
+
+export const AboutSection: FC = () => {
+  return (
+    <main className="flex flex-col max-h-screen min-h-screen overflow-y-scroll">
+      <section className="flex flex-col justify-center min-h-screen px-8 py-8 text-6xl font-extrabold text-white">
+        <h1>
+          <span className="text-salsa">HI</span>! I&apos;m Hayden Isler
+        </h1>
+
+        <p className="mt-4 text-lg font-medium">
+          A full-stack web developer in Austin, TX. I love music, dogs, and
+          programming. Let&apos;s build something together!
+        </p>
+      </section>
+    </main>
+  );
+};
