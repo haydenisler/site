1aa4817804758df2e0a936bd342cbdbf21e0b1f7
Hayden Isler <me@haydenisler.com>
2022-03-06 23:50:39 -0600

    feature: mobile text animation

--- components/AboutSection/AboutSection.tsx
+++ components/AboutSection/AboutSection.tsx
@@ -4,19 +4,67 @@ import type { FC } from 'react';
 
 import { Avatar } from '..';
 
-const animationVariants: {
-  [key: string]: Variants;
+const variants: {
+  [x: string]: Variants;
 } = {
-  paragraph: {
+  imVariant: {
     start: {
       opacity: 0,
-      y: 100,
     },
     end: {
       opacity: 1,
+      transition: {
+        delay: 1,
+      },
+    },
+  },
+  exclaimationVariant: {
+    start: {
+      opacity: 1,
+      x: 37,
+      y: -120,
+    },
+    end: {
+      opacity: 0,
+      x: 51,
+      y: -120,
+      transition: {
+        delay: 1,
+      },
+    },
+  },
+  hVariant: {
+    start: {
+      y: -40,
+    },
+    end: {
       y: 0,
       transition: {
-        delay: 2,
+        delay: 1,
+      },
+    },
+  },
+  iVariant: {
+    start: {
+      y: -80,
+      x: 27,
+    },
+    end: {
+      y: 0,
+      x: 0,
+      transition: {
+        delay: 1,
+      },
+    },
+  },
+  subVariant: {
+    start: {
+      opacity: 0,
+    },
+    end: {
+      opacity: 1,
+      transition: {
+        delay: 1,
       },
     },
   },
@@ -24,18 +72,45 @@ const animationVariants: {
 
 export const AboutSection: FC = () => {
   return (
-    <section className="flex flex-col justify-center min-h-screen px-8 py-8 text-6xl font-extrabold text-white">
+    <section className="flex flex-col items-center min-h-screen px-8 py-8 space-y-8 text-4xl font-extrabold text-white">
+      <h1 className="relative min-w-full">
+        <motion.div variants={variants.imVariant} initial="start" animate="end">
+          I&apos;m
+        </motion.div>
+        <motion.div variants={variants.hVariant} initial="start" animate="end">
+          H
+          <motion.span
+            variants={variants.subVariant}
+            initial="start"
+            animate="end"
+          >
+            ayden
+          </motion.span>
+        </motion.div>
+        <motion.div variants={variants.iVariant} initial="start" animate="end">
+          I
+          <motion.span
+            variants={variants.subVariant}
+            initial="start"
+            animate="end"
+          >
+            sler.
+          </motion.span>
+        </motion.div>
+        <motion.div
+          className="absolute"
+          variants={variants.exclaimationVariant}
+          initial="start"
+          animate="end"
+        >
+          !
+        </motion.div>
+      </h1>
       <Avatar />
-
-      <motion.p
-        className="mt-4 text-lg font-medium"
-        variants={animationVariants.paragraph}
-        initial="start"
-        animate="end"
-      >
+      <p className="mt-4 text-lg font-medium">
         A full-stack web developer in Austin, TX. I love music, dogs, and
-        programming. Let&apos;s build something together!
-      </motion.p>
+        programming. Let&apos;s build something awesome together!
+      </p>
     </section>
   );
 };
--- components/Avatar/Avatar.tsx
+++ components/Avatar/Avatar.tsx
@@ -1,13 +1,26 @@
+import type { Variants } from 'framer-motion';
+import { motion } from 'framer-motion';
 import type { FC } from 'react';
 
-export const Avatar: FC = () => {
+export interface AvatarProps {
+  variants?: Variants;
+  initial?: string;
+  animate?: string;
+}
+
+export const Avatar: FC<AvatarProps> = ({ variants, initial, animate }) => {
   return (
-    <div className="flex items-center justify-center rounded-full bg-gradient-to-br from-salsa via-tangerine to-sunflower w-52 h-52">
-      <div className="flex items-center justify-center w-[12.5rem] h-[12.5rem] rounded-full bg-rich-black">
-        <div className="flex items-center justify-center w-48 h-48 rounded-full bg-cadet">
-          <span className="text-8xl">👹</span>
+    <motion.div
+      variants={variants}
+      initial={initial}
+      animate={animate}
+      className="flex items-center justify-center rounded-full w-44 h-44 bg-gradient-to-br from-salsa via-tangerine to-sunflower"
+    >
+      <div className="flex items-center justify-center w-[10.5rem] h-[10.5rem] rounded-full bg-rich-black">
+        <div className="flex items-center justify-center w-40 h-40 rounded-full bg-cadet">
+          <span className="text-7xl">👹</span>
         </div>
       </div>
-    </div>
+    </motion.div>
   );
 };
