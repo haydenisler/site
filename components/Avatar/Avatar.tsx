811201762c5cc2bb04206b7c30bbd55d04aea5f1
Hayden Isler <me@haydenisler.com>
2022-03-06 22:32:09 -0600

    revert: avatar bg animations

--- components/Avatar/Avatar.tsx
+++ components/Avatar/Avatar.tsx
@@ -1,44 +1,13 @@
-import type { Variants } from 'framer-motion';
-import { motion } from 'framer-motion';
 import type { FC } from 'react';
 
 export const Avatar: FC = () => {
-  const animation: Variants = {
-    start: {
-      transform: 'rotate(0deg)',
-    },
-    end: {
-      transform: 'rotate(360deg)',
-      transition: {
-        duration: 5,
-        repeat: Infinity,
-      },
-    },
-  };
-
   return (
-    <div className="relative flex items-center justify-center flex-grow-0">
-      <div className="z-10 flex items-center justify-center w-[12.5rem] h-[12.5rem] rounded-full bg-rich-black">
+    <div className="flex items-center justify-center rounded-full bg-gradient-to-br from-salsa via-tangerine to-sunflower w-52 h-52">
+      <div className="flex items-center justify-center w-[12.5rem] h-[12.5rem] rounded-full bg-rich-black">
         <div className="flex items-center justify-center w-48 h-48 rounded-full bg-cadet">
           <span className="text-8xl">👹</span>
         </div>
       </div>
-      <motion.div
-        className="absolute z-0 flex items-center justify-center rounded-full bg-gradient-to-br from-salsa via-tangerine to-sunflower w-52 h-52"
-        variants={animation}
-        initial="start"
-        animate="end"
-      />
     </div>
   );
 };
-
-/*
- ** <div className="flex items-center justify-center rounded-full bg-gradient-to-br from-salsa via-tangerine to-sunflower w-52 h-52">
- **   <div className="flex items-center justify-center w-[12.5rem] h-[12.5rem] rounded-full bg-rich-black">
- **     <div className="flex items-center justify-center w-48 h-48 rounded-full bg-cadet">
- **       <span className="text-8xl">👹</span>
- **     </div>
- **   </div>
- ** </div>
- */
