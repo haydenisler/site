169a149f2bd6810d9fee071f188720897f84a9ff
Hayden Isler <me@haydenisler.com>
2024-03-30 04:32:13 -0500

    basic app setup + color vars

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import postcssNested from 'postcss-nested';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/site/',
+  base: '/',
+  css: {
+    modules: {
+      localsConvention: 'camelCaseOnly',
+    },
+    postcss: {
+      plugins: [postcssNested],
+    },
+  },
   plugins: [react()],
-})
+});
